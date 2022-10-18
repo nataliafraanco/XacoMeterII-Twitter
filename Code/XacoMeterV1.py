59d92bb81a93e03baafc1c01a6b09cec9a494de3
Natalia Franco <nataliafrancopeci@gmail.com>
2022-10-18 20:49:04 +0200

    #10

--- Code/XacoMeterV1.py
+++ Code/XacoMeterV1.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template
+import pandas as pd #Para poder almacenar datos en dataframes
+#import pyodbc #Proporciona herramientas para conectarse a una base de datos SQL
 import tweepy
 import credencialesAPITwitter
 from os import remove
@@ -7,23 +9,43 @@ import os
 #Para saber donde buscar los archivos
 app = Flask(__name__)
 
-auth = tweepy.OAuth1UserHandler(
-  credencialesAPITwitter.API_KEY,
-   credencialesAPITwitter.API_KEY_SECRET,
-   credencialesAPITwitter.ACCESS_TOKEN,
-   credencialesAPITwitter.ACCESS_TOKEN_SECRET
-)
-#auth = tweepy.OAuthHandler(credencialesAPITwitter.API_KEY, credencialesAPITwitter.API_KEY_SECRET)
-#auth.set_access_token(credencialesAPITwitter.ACCESS_TOKEN, credencialesAPITwitter.ACCESS_TOKEN_SECRET)
+#auth = tweepy.OAuth1UserHandler(
+#  credencialesAPITwitter.API_KEY,
+#   credencialesAPITwitter.API_KEY_SECRET,
+#   credencialesAPITwitter.ACCESS_TOKEN,
+#   credencialesAPITwitter.ACCESS_TOKEN_SECRET
+#)
+auth = tweepy.OAuthHandler(credencialesAPITwitter.API_KEY, credencialesAPITwitter.API_KEY_SECRET)
+auth.set_access_token(credencialesAPITwitter.ACCESS_TOKEN, credencialesAPITwitter.ACCESS_TOKEN_SECRET)
 
 api = tweepy.API(auth)
 client = tweepy.Client (bearer_token = credencialesAPITwitter.BEARER_TOKEN, consumer_key=credencialesAPITwitter.API_KEY,consumer_secret=credencialesAPITwitter.API_KEY_SECRET,access_token=credencialesAPITwitter.ACCESS_TOKEN,access_token_secret= credencialesAPITwitter.ACCESS_TOKEN_SECRET)
 
-def BusquedaTweets(PalabraClave):
 
-	tweets = client.search_recent_tweets (query=PalabraClave)
-	#tweets = api.search_tweets(q="PalabraClave") 
- 	#tweets = client.search_recent_tweets(query=PalabraClave, max_results=100))
+def BusquedaTweets(PalabraClave):
+	tweets = client.search_recent_tweets (query=PalabraClave,
+                                    tweet_fields = ["created_at", "text"],
+                                    #user_fields = ["username", "location", "verified", "description"],
+                                    max_results = 10,)
+ 
+	#print(len(tweets.includes["users"]))
+ 
+	tweetsInformation = []
+	for tweet in tweets.data: # , user in zip (tweets.data, tweets.includes["users"]):
+		tweetEachInformation = {
+		'created_at': tweet.created_at,
+        'text': tweet.text,
+        #'username': user.username,
+        #'location': user.location,
+        #'verified': user.verified,
+        #'description': user.description
+		}
+		tweetsInformation.append(tweetEachInformation)
+
+	tweetsDataFramework = pd.DataFrame(tweetsInformation)
+	print(tweetsDataFramework.head())
+	return ("DataFrame creado")
+	"""
 	if tweets.data == None:
 		return("No hay datos esta semana sobre la búsqueda")
 	else:
@@ -35,7 +57,7 @@ def BusquedaTweets(PalabraClave):
 			f.write(tweet.text + '\n')
 			f.close
 		return ("Los tweets están almacenados en busqueda.txt")
-
+	"""
 
 @app.route("/")
 def home():
