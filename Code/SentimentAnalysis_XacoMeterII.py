23f3c2bbc9a082b41ee614f65939ee94d87fe221
Natalia Franco <56863508+nataliafraanco@users.noreply.github.com>
2023-02-09 18:17:21 +0100

    Create SentimentAnalysis_XacoMeterII.py

---
+++ Code/SentimentAnalysis_XacoMeterII.py
@@ -0,0 +1,26 @@
+from sentiment_analysis_spanish import sentiment_analysis
+
+def SentimentAnalysis(conn,curs):
+    sentiment = sentiment_analysis.SentimentAnalysisSpanish()
+    curs.execute("SELECT Tweet_Id, Tweet_Texto FROM TWEETS_PATRIMONIOS")
+    filas=curs.fetchall()
+    for row in filas:
+        id = row[0]
+        texto = row[1]
+        sentimiento = sentiment.sentiment(texto)
+        curs.execute("UPDATE TWEETS_PATRIMONIOS SET Tweet_SentimentAnalysis = %s WHERE Tweet_Id = %s", (sentimiento, id))
+        conn.commit()
+        
+     
+def SentimentAnalysisPatrimonio(conn,curs):
+    sentiment = sentiment_analysis.SentimentAnalysisSpanish()
+    curs.execute("SELECT Tweet_Id, Tweet_Texto FROM TWEETS_PATRIMONIOS WHERE Tweet_SentimentAnalysis is NULL;")
+    filas=curs.fetchall()
+    for row in filas:
+        id = row[0]
+        texto = row[1]
+        sentimiento = sentiment.sentiment(texto)
+        curs.execute("UPDATE TWEETS_PATRIMONIOS SET Tweet_SentimentAnalysis = %s WHERE Tweet_Id = %s", (sentimiento, id))
+    conn.commit()
+    
+    
